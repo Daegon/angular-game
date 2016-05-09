29a27e3b63ae8090e94b34debaf5c13c32fa9324
Zimm <romsssa@gmail.com>
2016-05-09 08:09:42 +0300

    Init

---
+++ src/AngularGame/GameDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Framework.Configuration;
+using System.Data.Entity;
+using AngularGame.Models;
+
+namespace AngularGame
+{
+    [DbConfigurationType(typeof(GameDbConfiguration))]
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(IConfiguration config)
+            : base(config.Get("Data:DefaultConnection:ConnectionString"))
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
